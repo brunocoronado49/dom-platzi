d64196ce214d05119fa3903196c824ef8e2634c4
Bruno Coronado <francorangelcoronado@gmail.com>
2023-04-19 13:26:03 -0600

    Manipulacion del DOM

--- src/index.js
+++ src/index.js
@@ -3,4 +3,34 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const urlBase = "https://platzi-avo.vercel.app"
+const url = urlBase + "/api/avo"
+
+const appNode = document.querySelector("#app")
+
+window.fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        const todosLosItems = []
+        data.data.forEach(element => {
+            console.log(element.name);
+            
+            const imagen = document.createElement("img")
+            imagen.src = urlBase + element.image
+
+            const title = document.createElement("h2")
+            title.textContent = element.name
+
+            const price = document.createElement("div")
+            price.textContent = element.price
+
+            const container = document.createElement("div")
+            container.append(imagen, title, price)
+
+            todosLosItems.push(container)
+        });
+
+        appNode.append(...todosLosItems)
+    });
+
